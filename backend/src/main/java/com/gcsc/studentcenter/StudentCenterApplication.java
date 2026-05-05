e7ca04ea0e7fbfe8c5f11bc75b3546d188443700
MakiWinster <makiiiiiiiho@gmail.com>
2026-05-06 04:48:54 +0800

    I think this is ending bannnnnnner!

--- backend/src/main/java/com/gcsc/studentcenter/StudentCenterApplication.java
+++ backend/src/main/java/com/gcsc/studentcenter/StudentCenterApplication.java
@@ -1,7 +1,9 @@
 package com.gcsc.studentcenter;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -10,5 +12,19 @@ public class StudentCenterApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(StudentCenterApplication.class, args);
+  }
+
+  @Bean
+  ApplicationRunner printBanner() {
+    return args -> {
+      var banner = """
+                        ____  ____    _    ___
+                       | __ )|  _ \\  / \\  |_ _|
+                       |  _ \\| | | |/ _ \\  | |
+                       | |_) | |_| / ___ \\ | |
+                       |____/|____/_/   \\_\\___|
+          """;
+      System.out.println(banner);
+    };
   }
 }
